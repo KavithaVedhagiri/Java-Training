b0c033f53687ea69cebcf17d3fff5366296fedcf
KavithaVedhagiri <kavithavedhagiri@gmail.com>
2022-10-18 19:28:54 +0530

    Modules.java

---
+++ Assignment/Modules.java
@@ -0,0 +1,6 @@
+public class Modules{  
+static int module(int a,int b)
+{
+return a%b;
+}  
+}
---
+++ Assignment/TestOverloading2.java
@@ -0,0 +1,7 @@
+class TestOverloading2{  
+
+public static void main(String[] args)
+{  
+System.out.println(Modules.module(20,35));  
+}
+}  
