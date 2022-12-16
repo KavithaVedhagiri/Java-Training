be72011d93a555b48a847455194102bc66df3a9d
KavithaVedhagiri <kavithavedhagiri@gmail.com>
2022-12-16 23:30:35 +0530

    Array

---
+++ ArrayProgram/NegativeValue.java
@@ -0,0 +1,10 @@
+class NegativeValue{
+public static void main(String[] args){
+int[] ar = {10,-20,-30,-40,50,60}; 
+		for(int i=0; i<ar.length; i++)
+		{
+			if(ar[i]<0)
+		System.out.print(ar[i] + " "); 
+		}
+	}
+}
